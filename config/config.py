3364a172e2910509ce4dda0162a2a6e59d3f3b5a
!DegGixM! <97856367+DegGixM@users.noreply.github.com>
2022-05-03 18:22:07 +0400

    Update config.py

--- config/config.py
+++ config/config.py
@@ -67,6 +67,11 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", None
 )  # Example:- https://t.me/TheYukki
+
+# Only  Links formats are  accepted for this Var value.
+HEWartTV = getenv(
+    "HEWartTV", None
+)  # Example:- https://t.me/HEWartTV
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", None
 )  # Example:- https://t.me/YukkiSupport
